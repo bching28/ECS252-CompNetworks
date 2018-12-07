432743f26012bd4c2cbd0242b817c1135a65b077
Bryan <bryanching7373@gmail.com>
2018-12-06 17:05:18 -0800

    added plotting code

--- backoff_algorithms.py
+++ backoff_algorithms.py
@@ -2,22 +2,26 @@ import random
 import simpy
 import math
 import time
+import matplotlib.pyplot as plt
 
 RANDOM_SEED = 29
-SIM_TIME = 100000
+SIM_TIME = 1000000
 NUM_HOSTS = 10
 SLOT_TIME = 1
+LAMBDA = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
+list_throughput = []
 
 class stats:
     def __init__(self):
         self.successful_slots = 0
         self.total_slots = 0
         self.num_collisions = 0
-        self.wasted_slots = 0
+        self.empty_slots = 0
 
-    def throughput(self):
+    def throughput(self, arrival_rate):
         throughput = float(self.successful_slots)/self.total_slots
-        print 'Throughput:', throughput
+        list_throughput.append(throughput)
+        print 'Lambda:', arrival_rate, ' |  Throughput:', throughput
 
 class node:
     def __init__(self, env, arrival_rate):
@@ -25,7 +29,7 @@ class node:
         self.arrival_rate = arrival_rate
         self.N = 0 # num of retransmission times
         self.S = 0 # slot num of next transmission
-        self.L = 0 # num of pkts in queue
+        self.L = 0 # num of pkts in queue; L is the length of the queue
         self.server = simpy.Resource(env, capacity = 1)
 
     def process_packet(self, env):
@@ -44,12 +48,12 @@ class node:
             #env.process(self.process_packet(env, new_packet)) # you don't process packet until empty slot
 
 
-    # implement binary exponential backoff
+    # implement Binary Exponential backoff
     def binary_exp_backoff(self):
         # increment retransmission attempts
         self.N += 1
 
-        # perform binary exponential backoff computation
+        # perform Binary Exponential backoff computation
         K = min(self.N, 10)
         r = random.randint(0, (2**K)+1)
         self.S += r
@@ -64,10 +68,10 @@ class node:
         self.S += r
 
 class server:
-    def __init__(self, env, arrival_rate, list_of_hosts, backoff_algorithm, statistics):
+    def __init__(self, env, arrival_rate, hosts, backoff_algorithm, statistics):
         self.env = env
         self.arrival_rate = arrival_rate
-        self.list_of_hosts = list_of_hosts
+        self.hosts = hosts
         self.backoff_algorithm = backoff_algorithm
         self.statistics = statistics
         self.curr_slot_number = 0 # also the total number of slots
@@ -76,13 +80,12 @@ class server:
         yield self.env.timeout(100) # yield time for packets to initially arrive
         while True:
             yield self.env.timeout(SLOT_TIME) # yield periodically to make up every slot time
-            num_transmissions = 0 # number of transmission at any given time
-
             # check for simultaneous packet transmissions (i.e. if it is time for host to send)
             # check if host's queue has packets
             transmitting_hosts = []
+            num_transmissions = 0 # number of transmission at any given time
             for h in range(NUM_HOSTS):
-                if (self.list_of_hosts[h].L > 0 and self.list_of_hosts[h].S == self.curr_slot_number):
+                if (self.hosts[h].L > 0 and self.hosts[h].S == self.curr_slot_number):
                     transmitting_hosts.append(h)
                     num_transmissions += 1
 
@@ -92,16 +95,16 @@ class server:
 
             if num_transmissions == 1: # no collisions
                 self.statistics.successful_slots += 1
-                self.env.process(self.list_of_hosts[one_active_host_index].process_packet(self.env))
+                self.env.process(self.hosts[one_active_host_index].process_packet(self.env))
             elif num_transmissions > 1: # collisions
                 self.statistics.num_collisions += 1
                 for h in transmitting_hosts:
-                    if self.backoff_algorithm == 'binary exponential':
-                        self.list_of_hosts[h].binary_exp_backoff()
-                    elif self.backoff_algorithm == 'linear':
-                        self.list_of_hosts[h].linear_backoff()
+                    if self.backoff_algorithm == 'Binary Exponential':
+                        self.hosts[h].binary_exp_backoff()
+                    elif self.backoff_algorithm == 'Linear':
+                        self.hosts[h].linear_backoff()
             else: # num_transmissions < 1; wasted slot
-                self.statistics.wasted_slots += 1
+                self.statistics.empty_slots += 1
 
             self.curr_slot_number += 1 # increment slots for each iteration
             self.statistics.total_slots = self.curr_slot_number
@@ -109,31 +112,56 @@ class server:
             # for hosts that don't transmit, update their next transmission attempt
             # helps with when L=0 at the start of the simulation
             for h in range(NUM_HOSTS):
-                if ((h not in transmitting_hosts) and (self.list_of_hosts[h].S < self.curr_slot_number)):
-                    self.list_of_hosts[h].S = self.curr_slot_number
+                if ((h not in transmitting_hosts) and (self.hosts[h].S < self.curr_slot_number)):
+                    self.hosts[h].S = self.curr_slot_number
+
+def plot_throughput(algorithm):
+    title = algorithm + ' Backoff Throughput'
+    plt.plot(LAMBDA, list_throughput)
+    plt.title(title)
+    plt.xlabel('lambda (arrival rate)')
+    plt.ylabel('throughput')
+    if algorithm == 'Binary Exponential':
+        plt.text(0.06, 0.3, 'NUM_HOSTS=10', fontsize=10)
+        plt.text(0.06, 0.25, 'SIM_TIME=1000000', fontsize=10)
+        plt.text(0.06, 0.2, 'SLOT_TIME=1', fontsize=10)
+    elif algorithm == 'Linear':
+        plt.text(0.06, 0.14, 'NUM_HOSTS=10', fontsize=10)
+        plt.text(0.06, 0.13, 'SIM_TIME=1000000', fontsize=10)
+        plt.text(0.06, 0.12, 'SLOT_TIME=1', fontsize=10)
+    figure_name = 'Ching_' + algorithm + '.png'
+    plt.savefig(figure_name)
+    plt.clf()
 
 def main():
+    global list_throughput
     # lambda = [0.01 ... 0.09] with step size of 0.01
-    for algorithm in ['binary exponential', 'linear']:
-        print 'Performing', algorithm, 'backoff algorithm'
-        for arrival_rate in [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]:
+    for algorithm in ['Binary Exponential', 'Linear']:
+        print 'Performing', algorithm, 'Backoff Algorithm'
+        list_throughput = []
+        for arrival_rate in LAMBDA:
             statistics = stats()
             env = simpy.Environment()
-            list_of_hosts = []
+            hosts = []
 
             # initializing hosts
             for h in range(NUM_HOSTS):
-                list_of_hosts.append(node(env, arrival_rate))
+                hosts.append(node(env, arrival_rate))
 
             # have packets arrive simultaneously
             for h in range(NUM_HOSTS):
-                env.process(list_of_hosts[h].packets_arrival(env))
+                env.process(hosts[h].packets_arrival(env))
 
             backoff_algorithm = algorithm
-            router = server(env, arrival_rate, list_of_hosts, backoff_algorithm, statistics)
+            router = server(env, arrival_rate, hosts, backoff_algorithm, statistics)
             env.process(router.run())
             env.run(until=SIM_TIME)
-            statistics.throughput()
+            statistics.throughput(arrival_rate)
+
+        plot_throughput(algorithm)
+            
+
+        
 
 
 if __name__ == '__main__':
